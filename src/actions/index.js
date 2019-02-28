e8434cd2ec4e03084af05c90cdfc8754a3cd4bc1
Rachel Hofer <rachelhofer@rachels-mbp.home>
2019-02-27 20:33:54 -0500

    done

---
+++ src/actions/index.js
@@ -0,0 +1,29 @@
+import _ from 'lodash';
+import jsonPlaceholder from '../apis/jsonPlaceHolder';
+
+export const fetchPosts = () => {  //action creator
+    return async (dispatch) => {
+        const response = await jsonPlaceholder.get('/posts');
+        dispatch({ type: 'FETCH_POSTS', payload: response.data})
+    };
+};
+
+
+// above is the same as below, just less code
+
+// export const fetchPosts = () => async dispatch => {
+//     const response = await jsonPlaceholder.get('.posts');
+//     dispatch({ type: 'FETCH_POSTS', payload: response})
+// };
+
+
+
+export const fetchUser = (id) => dispatch => {
+    _fetchUser(id, dispatch);
+};
+
+const _fetchUser = _.memoize(async (id, dispatch) => {
+    const response = await jsonPlaceholder.get(`/users/${id}`);
+
+    dispatch({type: 'FETCH_USER', payload: response.data });
+}); //to only make axios get request when there is a NEW userID. Reduced GET calls from 100 down to 10.
